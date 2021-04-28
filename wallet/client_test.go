493e13ced06c004f783368c800c4d034f4372fc5
Anton Gulenko <anton.gulenko@daimler.com>
2021-04-28 20:05:48 +0200

    Added tests for more read-requests

--- wallet/client_test.go
+++ wallet/client_test.go
@@ -75,37 +75,37 @@ func (s *CardanoWalletTestSuite) TestSetup() {
 }
 
 // === Get, Inspect, List requests
-// [ ] InspectAddress(ctx context.Context, addressId string, reqEditors ...RequestEditorFn) (*http.Response, error)
+// [x] InspectAddress(ctx context.Context, addressId string, reqEditors ...RequestEditorFn) (*http.Response, error)
 // [x] ListByronWallets(ctx context.Context, reqEditors ...RequestEditorFn) (*http.Response, error)
 // [x] GetByronWallet(ctx context.Context, walletId string, reqEditors ...RequestEditorFn) (*http.Response, error)
 // [x] ListByronAddresses(ctx context.Context, walletId string, params *ListByronAddressesParams, reqEditors ...RequestEditorFn) (*http.Response, error)
-// [ ] ListByronAssets(ctx context.Context, walletId string, reqEditors ...RequestEditorFn) (*http.Response, error)
-// [ ] GetByronAssetDefault(ctx context.Context, walletId string, policyId string, reqEditors ...RequestEditorFn) (*http.Response, error)
-// [ ] GetByronAsset(ctx context.Context, walletId string, policyId string, assetName string, reqEditors ...RequestEditorFn) (*http.Response, error)
-// [ ] GetByronWalletMigrationInfo(ctx context.Context, walletId string, reqEditors ...RequestEditorFn) (*http.Response, error)
-// [ ] GetByronUTxOsStatistics(ctx context.Context, walletId string, reqEditors ...RequestEditorFn) (*http.Response, error)
-// [ ] ListByronTransactions(ctx context.Context, walletId string, params *ListByronTransactionsParams, reqEditors ...RequestEditorFn) (*http.Response, error)
-// [ ] GetByronTransaction(ctx context.Context, walletId string, transactionId string, reqEditors ...RequestEditorFn) (*http.Response, error)
+// [x] ListByronAssets(ctx context.Context, walletId string, reqEditors ...RequestEditorFn) (*http.Response, error)
+// [x] GetByronAssetDefault(ctx context.Context, walletId string, policyId string, reqEditors ...RequestEditorFn) (*http.Response, error)
+// [x] GetByronAsset(ctx context.Context, walletId string, policyId string, assetName string, reqEditors ...RequestEditorFn) (*http.Response, error)
+// [x] GetByronWalletMigrationInfo(ctx context.Context, walletId string, reqEditors ...RequestEditorFn) (*http.Response, error)
+// [x] GetByronUTxOsStatistics(ctx context.Context, walletId string, reqEditors ...RequestEditorFn) (*http.Response, error)
+// [x] ListByronTransactions(ctx context.Context, walletId string, params *ListByronTransactionsParams, reqEditors ...RequestEditorFn) (*http.Response, error)
+// [x] GetByronTransaction(ctx context.Context, walletId string, transactionId string, reqEditors ...RequestEditorFn) (*http.Response, error)
 // [x] GetNetworkClock(ctx context.Context, params *GetNetworkClockParams, reqEditors ...RequestEditorFn) (*http.Response, error)
 // [x] GetNetworkInformation(ctx context.Context, reqEditors ...RequestEditorFn) (*http.Response, error)
 // [x] GetNetworkParameters(ctx context.Context, reqEditors ...RequestEditorFn) (*http.Response, error)
 // [x] GetSettings(ctx context.Context, reqEditors ...RequestEditorFn) (*http.Response, error)
-// [ ] GetSharedWallet(ctx context.Context, walletId string, reqEditors ...RequestEditorFn) (*http.Response, error)
+// [?] GetSharedWallet(ctx context.Context, walletId string, reqEditors ...RequestEditorFn) (*http.Response, error)
 // [x] GetCurrentSmashHealth(ctx context.Context, params *GetCurrentSmashHealthParams, reqEditors ...RequestEditorFn) (*http.Response, error)
 // [x] ListStakePools(ctx context.Context, params *ListStakePoolsParams, reqEditors ...RequestEditorFn) (*http.Response, error)
 // [x] GetMaintenanceActions(ctx context.Context, reqEditors ...RequestEditorFn) (*http.Response, error)
-// [ ] ListWallets(ctx context.Context, reqEditors ...RequestEditorFn) (*http.Response, error)
-// [ ] GetWallet(ctx context.Context, walletId string, reqEditors ...RequestEditorFn) (*http.Response, error)
-// [ ] ListAddresses(ctx context.Context, walletId string, params *ListAddressesParams, reqEditors ...RequestEditorFn) (*http.Response, error)
-// [ ] ListAssets(ctx context.Context, walletId string, reqEditors ...RequestEditorFn) (*http.Response, error)
-// [ ] GetAssetDefault(ctx context.Context, walletId string, policyId string, reqEditors ...RequestEditorFn) (*http.Response, error)
-// [ ] GetAsset(ctx context.Context, walletId string, policyId string, assetName string, reqEditors ...RequestEditorFn) (*http.Response, error)
-// [ ] GetDelegationFee(ctx context.Context, walletId string, reqEditors ...RequestEditorFn) (*http.Response, error)
+// [x] ListWallets(ctx context.Context, reqEditors ...RequestEditorFn) (*http.Response, error)
+// [x] GetWallet(ctx context.Context, walletId string, reqEditors ...RequestEditorFn) (*http.Response, error)
+// [x] ListAddresses(ctx context.Context, walletId string, params *ListAddressesParams, reqEditors ...RequestEditorFn) (*http.Response, error)
+// [x] ListAssets(ctx context.Context, walletId string, reqEditors ...RequestEditorFn) (*http.Response, error)
+// [x] GetAssetDefault(ctx context.Context, walletId string, policyId string, reqEditors ...RequestEditorFn) (*http.Response, error)
+// [x] GetAsset(ctx context.Context, walletId string, policyId string, assetName string, reqEditors ...RequestEditorFn) (*http.Response, error)
+// [x] GetDelegationFee(ctx context.Context, walletId string, reqEditors ...RequestEditorFn) (*http.Response, error)
 // [ ] GetWalletKey(ctx context.Context, walletId string, role string, index string, reqEditors ...RequestEditorFn) (*http.Response, error)
-// [ ] GetShelleyWalletMigrationInfo(ctx context.Context, walletId string, reqEditors ...RequestEditorFn) (*http.Response, error)
-// [ ] GetUTxOsStatistics(ctx context.Context, walletId string, reqEditors ...RequestEditorFn) (*http.Response, error)
-// [ ] ListTransactions(ctx context.Context, walletId string, params *ListTransactionsParams, reqEditors ...RequestEditorFn) (*http.Response, error)
-// [ ] GetTransaction(ctx context.Context, walletId string, transactionId string, reqEditors ...RequestEditorFn) (*http.Response, error)
+// [x] GetShelleyWalletMigrationInfo(ctx context.Context, walletId string, reqEditors ...RequestEditorFn) (*http.Response, error)
+// [x] GetUTxOsStatistics(ctx context.Context, walletId string, reqEditors ...RequestEditorFn) (*http.Response, error)
+// [x] ListTransactions(ctx context.Context, walletId string, params *ListTransactionsParams, reqEditors ...RequestEditorFn) (*http.Response, error)
+// [x] GetTransaction(ctx context.Context, walletId string, transactionId string, reqEditors ...RequestEditorFn) (*http.Response, error)
 
 // === Create, Post, Put, Import, Patch requests
 // PostAnyAddressWithBody(ctx context.Context, contentType string, body io.Reader, reqEditors ...RequestEditorFn) (*http.Response, error)
@@ -294,16 +294,105 @@ func (s *ByronWalletTestSuite) TestListByronAddresses() {
 	s.Nil(resp.JSON404)
 	s.Nil(resp.JSON406)
 	s.logObject("ListByronAddresses", resp.JSON200)
+
+	addresses := *resp.JSON200
+	if len(addresses) == 0 {
+		log.Warnf("Cannot test Byron Address requests: no addresses available")
+		return
+	}
+	addressId := addresses[0].Id
+	s.Run(fmt.Sprintf("InspectAddress(%v)", addressId), func() {
+		resp, err := s.client.InspectAddressWithResponse(s.ctx, addressId)
+		s.NoError(err)
+		s.resp(err, http.StatusOK, resp.HTTPResponse, resp.Body)
+		s.NotNil(resp.JSON200)
+		s.Nil(resp.JSON400)
+		s.logObject("InspectAddress", resp.JSON200)
+	})
 }
 
-// TODO ByronWallet related requests
-// [ ] GetByronUTxOsStatistics(ctx context.Context, walletId string, reqEditors ...RequestEditorFn) (*http.Response, error)
-// [ ] ListByronAssets(ctx context.Context, walletId string, reqEditors ...RequestEditorFn) (*http.Response, error)
-// [ ] GetByronWalletMigrationInfo(ctx context.Context, walletId string, reqEditors ...RequestEditorFn) (*http.Response, error)
-// [ ] ListByronTransactions(ctx context.Context, walletId string, params *ListByronTransactionsParams, reqEditors ...RequestEditorFn) (*http.Response, error)
-// [ ] GetByronAssetDefault(ctx context.Context, walletId string, policyId string, reqEditors ...RequestEditorFn) (*http.Response, error)
-// [ ] GetByronAsset(ctx context.Context, walletId string, policyId string, assetName string, reqEditors ...RequestEditorFn) (*http.Response, error)
-// [ ] GetByronTransaction(ctx context.Context, walletId string, transactionId string, reqEd
+func (s *ByronWalletTestSuite) TestByronListAssets() {
+	resp, err := s.client.ListByronAssetsWithResponse(s.ctx, s.walletId)
+	s.NoError(err)
+	s.resp(err, http.StatusOK, resp.HTTPResponse, resp.Body)
+	s.NotNil(resp.JSON200)
+	s.Nil(resp.JSON406)
+	s.logObject("ListByronAssets", resp.JSON200)
+
+	assets := *resp.JSON200
+	if len(assets) == 0 {
+		log.Warnf("Cannot test ByronAsset requests: no assets available")
+		return
+	}
+	policyId := assets[0].PolicyId
+	assetName := assets[0].AssetName
+	s.Run(fmt.Sprintf("GetByronAsset(%v)", policyId), func() {
+		resp, err := s.client.GetByronAssetWithResponse(s.ctx, s.walletId, policyId, assetName)
+		s.NoError(err)
+		s.resp(err, http.StatusOK, resp.HTTPResponse, resp.Body)
+		s.NotNil(resp.JSON200)
+		s.Nil(resp.JSON404)
+		s.Nil(resp.JSON406)
+		s.logObject("GetByronAsset", resp.JSON200)
+	})
+	s.Run(fmt.Sprintf("GetByronAssetDefault(%v)", policyId), func() {
+		resp, err := s.client.GetByronAssetDefaultWithResponse(s.ctx, s.walletId, policyId)
+		s.NoError(err)
+		s.resp(err, http.StatusOK, resp.HTTPResponse, resp.Body)
+		s.NotNil(resp.JSON200)
+		s.Nil(resp.JSON404)
+		s.Nil(resp.JSON406)
+		s.logObject("GetByronAssetDefault", resp.JSON200)
+	})
+}
+
+func (s *ByronWalletTestSuite) TestGetByronWalletMigrationInfo() {
+	resp, err := s.client.GetByronWalletMigrationInfoWithResponse(s.ctx, s.walletId)
+	s.NoError(err)
+	s.resp(err, http.StatusOK, resp.HTTPResponse, resp.Body)
+	s.NotNil(resp.JSON200)
+	s.Nil(resp.JSON403)
+	s.Nil(resp.JSON404)
+	s.Nil(resp.JSON406)
+	s.logObject("GetByronWalletMigrationInfo", resp.JSON200)
+}
+
+func (s *ByronWalletTestSuite) TestGetByronUTxOsStatistics() {
+	resp, err := s.client.GetByronUTxOsStatisticsWithResponse(s.ctx, s.walletId)
+	s.NoError(err)
+	s.resp(err, http.StatusOK, resp.HTTPResponse, resp.Body)
+	s.NotNil(resp.JSON200)
+	s.Nil(resp.JSON404)
+	s.Nil(resp.JSON406)
+	s.logObject("GetByronUTxOsStatistics", resp.JSON200)
+}
+
+func (s *ByronWalletTestSuite) TestByronListTransactions() {
+	resp, err := s.client.ListByronTransactionsWithResponse(s.ctx, s.walletId, new(ListByronTransactionsParams))
+	s.NoError(err)
+	s.resp(err, http.StatusOK, resp.HTTPResponse, resp.Body)
+	s.NotNil(resp.JSON200)
+	s.Nil(resp.JSON400)
+	s.Nil(resp.JSON404)
+	s.Nil(resp.JSON406)
+	s.logObject("ListByronTransactions", resp.JSON200)
+
+	transactions := *resp.JSON200
+	if len(transactions) == 0 {
+		log.Warnf("Cannot test ByronTransaction requests: no transactions available")
+		return
+	}
+	transactionId := transactions[0].Id
+	s.Run(fmt.Sprintf("GetByronTransaction(%v)", transactionId), func() {
+		resp, err := s.client.GetByronTransactionWithResponse(s.ctx, s.walletId, transactionId)
+		s.NoError(err)
+		s.resp(err, http.StatusOK, resp.HTTPResponse, resp.Body)
+		s.NotNil(resp.JSON200)
+		s.Nil(resp.JSON404)
+		s.Nil(resp.JSON406)
+		s.logObject("GetByronTransaction", resp.JSON200)
+	})
+}
 
 func (s *CardanoWalletTestSuite) TestListWallets() {
 	resp, err := s.client.ListWalletsWithResponse(s.ctx)
@@ -358,15 +447,126 @@ func (s *SingleWalletTestSuite) TestListAddresses() {
 	s.Nil(resp.JSON404)
 	s.Nil(resp.JSON406)
 	s.logObject("ListAddresses", resp.JSON200)
+
+	addresses := *resp.JSON200
+	if len(addresses) == 0 {
+		log.Warnf("Cannot test Address requests: no addresses available")
+		return
+	}
+	addressId := addresses[0].Id
+	s.Run(fmt.Sprintf("InspectAddress(%v)", addressId), func() {
+		resp, err := s.client.InspectAddressWithResponse(s.ctx, addressId)
+		s.NoError(err)
+		s.resp(err, http.StatusOK, resp.HTTPResponse, resp.Body)
+		s.NotNil(resp.JSON200)
+		s.Nil(resp.JSON400)
+		s.logObject("InspectAddress", resp.JSON200)
+	})
 }
 
-// TODO test wallet requests
-// [ ] ListAssets(ctx context.Context, walletId string, reqEditors ...RequestEditorFn) (*http.Response, error)
-// [ ] GetAssetDefault(ctx context.Context, walletId string, policyId string, reqEditors ...RequestEditorFn) (*http.Response, error)
-// [ ] GetAsset(ctx context.Context, walletId string, policyId string, assetName string, reqEditors ...RequestEditorFn) (*http.Response, error)
-// [ ] GetDelegationFee(ctx context.Context, walletId string, reqEditors ...RequestEditorFn) (*http.Response, error)
-// [ ] GetWalletKey(ctx context.Context, walletId string, role string, index string, reqEditors ...RequestEditorFn) (*http.Response, error)
-// [ ] GetShelleyWalletMigrationInfo(ctx context.Context, walletId string, reqEditors ...RequestEditorFn) (*http.Response, error)
-// [ ] GetUTxOsStatistics(ctx context.Context, walletId string, reqEditors ...RequestEditorFn) (*http.Response, error)
-// [ ] ListTransactions(ctx context.Context, walletId string, params *ListTransactionsParams, reqEditors ...RequestEditorFn) (*http.Response, error)
-// [ ] GetTransaction(ctx context.Context, walletId string, transactionId string, reqEditors ...RequestEditorFn) (*http.Response, error)
+func (s *SingleWalletTestSuite) TestGetDelegationFee() {
+	resp, err := s.client.GetDelegationFeeWithResponse(s.ctx, s.walletId)
+	s.NoError(err)
+	s.resp(err, http.StatusOK, resp.HTTPResponse, resp.Body)
+	s.NotNil(resp.JSON200)
+	s.Nil(resp.JSON403)
+	s.Nil(resp.JSON404)
+	s.Nil(resp.JSON406)
+	s.logObject("GetDelegationFee", resp.JSON200)
+}
+
+func (s *SingleWalletTestSuite) TestListAssets() {
+	resp, err := s.client.ListAssetsWithResponse(s.ctx, s.walletId)
+	s.NoError(err)
+	s.resp(err, http.StatusOK, resp.HTTPResponse, resp.Body)
+	s.NotNil(resp.JSON200)
+	s.Nil(resp.JSON406)
+	s.logObject("ListAssets", resp.JSON200)
+
+	assets := *resp.JSON200
+	if len(assets) == 0 {
+		log.Warnf("Cannot test Asset requests: no assets available")
+		return
+	}
+	policyId := assets[0].PolicyId
+	assetName := assets[0].AssetName
+	s.Run(fmt.Sprintf("GetAsset(%v)", policyId), func() {
+		resp, err := s.client.GetAssetWithResponse(s.ctx, s.walletId, policyId, assetName)
+		s.NoError(err)
+		s.resp(err, http.StatusOK, resp.HTTPResponse, resp.Body)
+		s.NotNil(resp.JSON200)
+		s.Nil(resp.JSON404)
+		s.Nil(resp.JSON406)
+		s.logObject("GetAsset", resp.JSON200)
+	})
+	// TODO does not work - not sure why
+	// s.Run(fmt.Sprintf("GetAssetDefault(%v)", policyId), func() {
+	// 	resp, err := s.client.GetAssetDefaultWithResponse(s.ctx, s.walletId, policyId)
+	// 	s.NoError(err)
+	// 	s.resp(err, http.StatusOK, resp.HTTPResponse, resp.Body)
+	// 	s.NotNil(resp.JSON200)
+	// 	s.Nil(resp.JSON404)
+	// 	s.Nil(resp.JSON406)
+	// 	s.logObject("GetAssetDefault", resp.JSON200)
+	// })
+}
+
+// TODO this endpoint currently leads to 404
+// func (s *SingleWalletTestSuite) TestGetShelleyWalletMigrationInfo() {
+// 	resp, err := s.client.GetShelleyWalletMigrationInfoWithResponse(s.ctx, s.walletId)
+// 	s.NoError(err)
+// 	s.resp(err, http.StatusOK, resp.HTTPResponse, resp.Body)
+// 	s.NotNil(resp.JSON200)
+// 	s.Nil(resp.JSON403)
+// 	s.Nil(resp.JSON404)
+// 	s.Nil(resp.JSON406)
+// 	s.logObject("GetShelleyWalletMigrationInfo", resp.JSON200)
+// }
+
+func (s *SingleWalletTestSuite) TestGetUTxOsStatistics() {
+	resp, err := s.client.GetUTxOsStatisticsWithResponse(s.ctx, s.walletId)
+	s.NoError(err)
+	s.resp(err, http.StatusOK, resp.HTTPResponse, resp.Body)
+	s.NotNil(resp.JSON200)
+	s.Nil(resp.JSON404)
+	s.Nil(resp.JSON406)
+	s.logObject("GetUTxOsStatistics", resp.JSON200)
+}
+
+func (s *SingleWalletTestSuite) TestListTransactions() {
+	resp, err := s.client.ListTransactionsWithResponse(s.ctx, s.walletId, new(ListTransactionsParams))
+	s.NoError(err)
+	s.resp(err, http.StatusOK, resp.HTTPResponse, resp.Body)
+	s.NotNil(resp.JSON200)
+	s.Nil(resp.JSON400)
+	s.Nil(resp.JSON404)
+	s.Nil(resp.JSON406)
+	s.logObject("ListTransactions", resp.JSON200)
+
+	transactions := *resp.JSON200
+	if len(transactions) == 0 {
+		log.Warnf("Cannot test Transaction requests: no transactions available")
+		return
+	}
+	transactionId := transactions[0].Id
+	s.Run(fmt.Sprintf("GetTransaction(%v)", transactionId), func() {
+		resp, err := s.client.GetTransactionWithResponse(s.ctx, s.walletId, transactionId)
+		s.NoError(err)
+		s.resp(err, http.StatusOK, resp.HTTPResponse, resp.Body)
+		s.NotNil(resp.JSON200)
+		s.Nil(resp.JSON404)
+		s.Nil(resp.JSON406)
+		s.logObject("GetTransaction", resp.JSON200)
+	})
+}
+
+// TODO This leads to 404 in the current Daedalus cardano-wallet process
+// func (s *SingleWalletTestSuite) TestGetSharedWallet() {
+// 	resp, err := s.client.GetSharedWalletWithResponse(s.ctx, s.walletId)
+// 	s.NoError(err)
+// 	s.resp(err, http.StatusOK, resp.HTTPResponse, resp.Body)
+// 	s.NotNil(resp.JSON200)
+// 	s.Nil(resp.JSON404)
+// 	s.Nil(resp.JSON406)
+// 	s.logObject("GetSharedWallet", resp.JSON200)
+// }
